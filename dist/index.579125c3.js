5a7343003d591cb59646e367e4c669ed3c55c82a
Vlada Hudii <vlada.hudii@laba.com>
2024-03-14 17:36:27 +0200

    add

---
+++ dist/index.579125c3.js
@@ -0,0 +1,64 @@
+function UsersService(url) {
+    this.baseUrl = url;
+}
+UsersService.prototype.getAllUsers = async function() {
+    try {
+        const response = await fetch(this.baseUrl);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error("Error receive the list:", err);
+        return [];
+    }
+};
+UsersService.prototype.getUserById = async function(id) {
+    try {
+        const response = await fetch(`${this.baseUrl}/${id}`);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error("Error receive the id:", err);
+        return [];
+    }
+};
+UsersService.prototype.renderUsersList = function(list) {
+    const listElement = document.querySelector("#message-list");
+    list.forEach((value)=>{
+        const itemElement = document.createElement("li");
+        const imgUrlParams = new URLSearchParams({
+            w: 120,
+            h: 120
+        });
+        imgUrlParams.append("r", value.id);
+        itemElement.textContent = `
+    <figure> 
+    <img src="https://api.lorem.space/image/face?${imgUrlParams.toString}" /> 
+    <figcaption>
+    <span>${value.name}</span>
+    <span>${value.username}</span>
+    </figcaption>
+    </figure>
+   
+    `;
+        itemElement.addEventListener("click", async ()=>{
+            const detail = await this.getUserById(value.id);
+            const detailItem = document.createElement("p");
+            detailItem.textContent = `
+    Company: ${value.company.name},
+    Phone: ${detail.phone},
+    Email: ${detail.email},
+    Address: ${value.address.street}, ${value.address.suite}, ${value.address.city}, ${value.address.zipcode},
+     `;
+            itemElement.append(detailItem);
+        });
+        listElement.appendChild(itemElement);
+    });
+};
+const userService = new UsersService("https://jsonplaceholder.typicode.com/users");
+userService.getAllUsers().then((list)=>{
+    userService.renderUsersList(list);
+}).catch((err)=>{
+    console.error(err);
+});
+
+//# sourceMappingURL=index.579125c3.js.map
--- src/index.js
+++ src/index.js
@@ -1,80 +1,79 @@
 function UsersService(url) {
     this.baseUrl = url;
-  }
-  
-  
-  UsersService.prototype.getAllUsers = async function () {
+}
+
+
+UsersService.prototype.getAllUsers = async function () {
   try {
     const response = await fetch(this.baseUrl);
-    const data = response.json();
+    const data = await response.json();
     return data;
   } catch(err) {
     console.error('Error receive the list:', err);
     return [];
   }
-  }
-  
-  
-  UsersService.prototype.getUserById = async function(id) {
+}
+
+UsersService.prototype.getUserById = async function(id) {
     try {
         const response = await fetch(`${this.baseUrl}/${id}`);
-        const data = response.json();
+        const data = await response.json();
         return data;
       } catch(err) {
-        console.error('Error receive id:', err);
+        console.error('Error receive the id:', err);
         return [];
       }
-  }
-  
-  
-  UsersService.prototype.renderUsersList = function (list) {
+    }
+
+
+   UsersService.prototype.renderUsersList = function (list) {
    const listElement = document.querySelector('#message-list');
-  
-  
-   list.forEach((value) => {
+
+
+    list.forEach((value) => {
     const itemElement = document.createElement('li');
-  
-  
-    const imgUrlParams = new URLSearchParams({ w: 120, h: 120 });
-    imgUrlParams.append('r', value.id );
-  
-  
-    itemElement.innerHTML = `
-    <figure>
-    <img src="https://api.lorem.space/image/face?${imgUrlParams.toString()}" />    
+
+    const imgUrlParams = new URLSearchParams({ w: 120, h: 120});
+    imgUrlParams.append('r', value.id);
+
+    itemElement.textContent = `
+    <figure> 
+    <img src="https://api.lorem.space/image/face?${imgUrlParams.toString}" /> 
     <figcaption>
-    <span>${value.email}</span>
     <span>${value.name}</span>
+    <span>${value.username}</span>
     </figcaption>
     </figure>
    
     `;
-  
-  
-    itemElement.addEventListener( 'click', async () => {
-        const detail = await this.getUserById(value.id);
-        console.e.log(detail);
-    })
-  
-  
-    listElement.appendChild(itemElement);
-  
-  
+
+
+    itemElement.addEventListener('click', async () => {
+    const detail = await this.getUserById(value.id);
+    const detailItem = document.createElement('p');
+    detailItem.textContent = `
+    Company: ${value.company.name},
+    Phone: ${detail.phone},
+    Email: ${detail.email},
+    Address: ${value.address.street}, ${value.address.suite}, ${value.address.city}, ${value.address.zipcode},
+     `;
+
+    itemElement.append(detailItem);
     });
-  }
-  
-  
-  
-  
-  
-  
-  const userService = new UsersService('https://jsonplaceholder.typicode.com/users');
-  userService.getAllUsers()
+
+
+    listElement.appendChild(itemElement);
+
+
+    });
+}
+
+const userService = new UsersService('https://jsonplaceholder.typicode.com/users');
+userService.getAllUsers()
   .then(list => {
     userService.renderUsersList(list);
   })
   .catch(err => {
     console.error (err)
   })
-  
-  
+
